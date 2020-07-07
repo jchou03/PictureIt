6e3af87aa924f1114e79c473181ba6faa9974dd7
jchou21 <jaredhappy@gmail.com>
2020-07-07 13:25:45 -0700

    first iteration of sections of single page on app

--- pictureit/lib/main.dart
+++ pictureit/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-// color setup
+// variable setup for the app
+// color setups
 const backgroundColor = Color(0xFFE7FBF4);
 const headingColor = Colors.white;
 const boxColor = Color(0xFF94DFD2);
 const arrowColor = Color(0xFFFFF7BF);
+// padding, margin, and border radius setup
+const globalPadding = 15.0;
+const globalMargin = 15.0;
+const borderRadius = 10.0;
 
 void main() {
   runApp(MyApp());
@@ -15,24 +20,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PictureIt',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: backgroundColor,
+        primaryColor: headingColor,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'PictureIt'),
     );
   }
 }
@@ -71,40 +67,36 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // This method is rerun every time setState is called
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      backgroundColor: backgroundColor,
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          // an array of widgets for
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(globalPadding),
+              margin: EdgeInsets.all(globalMargin),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(borderRadius),
+                  color: Colors.white),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text('My Projects: '),
+                  Text('SamTrans - Leader'),
+                  Text('Homlessness - Leader'),
+                ],
+              ),
             ),
             Text(
               '$_counter',
@@ -120,4 +112,25 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// Function for creating containers for different parts of the app *unfinished*
+Container SectionCreation(String title, BuildContext context) {
+  Container item = Container(
+    width: MediaQuery.of(context).size.width,
+    padding: EdgeInsets.all(globalPadding),
+    margin: EdgeInsets.all(globalMargin),
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius), color: Colors.white),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text('My Projects: '),
+        Text('SamTrans - Leader'),
+        Text('Homlessness - Leader'),
+      ],
+    ),
+  );
+
+  return item;
 }
